26fce6bcfe7a5269c23013f6a199c572959c2539
michael <29688009+michael-EA@users.noreply.github.com>
2022-07-08 20:35:54 +0000

    Changed the vkax website url

--- src/dashd.cpp
+++ src/dashd.cpp
@@ -30,7 +30,7 @@
  *
  * \section intro_sec Introduction
  *
- * This is the developer documentation of the reference client for an experimental new digital currency called Vkax (https://www.vkax.tech/),
+ * This is the developer documentation of the reference client for an experimental new digital currency called Vkax (https://www.vkax.xyz/),
  * which enables instant payments to anyone, anywhere in the world. Vkax uses peer-to-peer technology to operate
  * with no central authority: managing transactions and issuing money are carried out collectively by the network.
  *
